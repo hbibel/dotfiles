bbaa8b655e879962934d51db626f1542063045b2
Hannes Bibel <hannes.bibel@gmail.com>
2025-08-12 20:40:16 +0200

    Add Snap and Usnap user commands to Neovim

--- .config/nvim/lua/custom/commands.lua
+++ .config/nvim/lua/custom/commands.lua
@@ -7,6 +7,11 @@ local function basic()
   -- I can't type
   vim.cmd("command! -nargs=* W w <args>")
   vim.cmd("command! -nargs=* Wq wq <args>")
+
+  -- Create a point in time to recover later, useful for small experiments
+  -- within a file which I want to undo in one step
+  vim.cmd("command! -nargs=0 Snap let snapshot=undotree().seq_last")
+  vim.cmd("command! -nargs=0 Usnap exe 'undo ' . snapshot")
 end
 
 local function lsp(bufnr)
