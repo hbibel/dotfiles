017de061a8befb2147470d21e7f9eb0b20321a6a
Hannes Bibel <hannes.bibel@gmail.com>
2025-09-15 19:04:06 +0200

    Configure more Python formatters

--- .config/nvim/lua/custom/plugins/conform.lua
+++ .config/nvim/lua/custom/plugins/conform.lua
@@ -1,44 +1,60 @@
 return {
   setup = function()
-    local isort_cmd
-    if require("custom.python").venv_in_project() then
-      isort_cmd = ".venv/bin/isort"
-    else
-      isort_cmd = "isort"
+    local venv_dir = require("custom.python").get_venv_dir()
+    local python_formatters = {}
+    if venv_dir ~= nil then
+      python_formatters = {
+        ruff_imports = {
+          command = venv_dir .. "/bin/ruff",
+          args = {
+            "check",
+            "--select", "I",
+            "--fix",
+            "--stdin-filename", "$FILENAME",
+            "-",
+          },
+        },
+        black = {
+          command = venv_dir .. "/bin/black",
+          args = {
+            "--stdin-filename", "$FILENAME",
+            "--quiet",
+            "-",
+          }
+        },
+        isort = {
+          command = venv_dir .. "/bin/isort",
+        },
+        ruff_format = {
+          command = venv_dir .. "/bin/ruff",
+        },
+      }
     end
 
-    local ruff_cmd
-    if require("custom.python").venv_in_project() then
-      ruff_cmd = ".venv/bin/ruff"
-    else
-      ruff_cmd = "ruff"
-    end
+    local formatters = vim.tbl_extend("error",
+      python_formatters,
+      {
+        terraform_fmt = {
+          command = "tofu",
+          args = { "fmt", "-no-color", "-" },
+        },
+      }
+    )
 
     require("conform").setup({
       format_on_save = {
         timeout_ms = 1500,
         lsp_fallback = true,
       },
-      log_level = vim.log.levels.DEBUG,
-      formatters = {
-        isort = {
-          command = isort_cmd,
-        },
-        ruff_format = {
-          command = ruff_cmd,
-        },
-        terraform_fmt = {
-          command = "tofu",
-          args = { "fmt", "-no-color", "-" },
-        },
-      },
+      log_level = vim.log.levels.INFO,
+      formatters = formatters,
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { "isort", "ruff_format" },
+        python = venv_dir and { "ruff_imports", "ruff_format" } or {},
         -- Scalafmt standalone is SLOOOOOOOOW, so we fallback to LSP formatting
         -- scala = { "scalafmt" },
         -- Go LSP formatting is fine for now, so no explicit config here
-        html = { { "djlint", "prettier" } },
+        html = { "prettier" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
         typescript = { "prettier" },
